acd9577e081bac04b6ee8ba53eea71855c93b3fe
Quy Le <quyle1703@gmail.com>
2017-10-21 07:54:49 +0700

    Quy commit trang cap dai

--- AppChamDiemVo/app/src/main/java/com/example/quyle/appchamdiemvo/ProfileActivity.java
+++ AppChamDiemVo/app/src/main/java/com/example/quyle/appchamdiemvo/ProfileActivity.java
@@ -1,8 +1,8 @@
 package com.example.quyle.appchamdiemvo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -34,7 +34,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             //closing this activity
             finish();
             //starting login activity
-            startActivity(new Intent(this, LoginActivity.class));
+            startActivity(new Intent(this, MainActivity.class));
         }
 
         //getting current user
